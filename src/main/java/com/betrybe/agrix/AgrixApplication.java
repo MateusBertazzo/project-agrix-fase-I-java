fcc6c0882caa0877aa45b1ad92d68ccfcb7a3115
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-11-17 15:08:26 -0300

    fix AgrixAplication

--- src/main/java/com/betrybe/agrix/AgrixApplication.java
+++ src/main/java/com/betrybe/agrix/AgrixApplication.java
@@ -3,8 +3,8 @@ package com.betrybe.agrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Component;
 
 /**
  * Application main class.
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 @SpringBootApplication
 @EntityScan("com.betrybe.agrix.models.entities")
 @EnableJpaRepositories("com.betrybe.agrix.models.repositories")
-@Component("com.betrybe.agrix")
+@ComponentScan("com.betrybe.agrix")
 public class AgrixApplication {
 
   public static void main(String[] args) {
