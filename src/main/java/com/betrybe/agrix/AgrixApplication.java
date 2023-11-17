3ea61cde033bd407a2f0787ef0e6ca3c0a191c95
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-11-17 15:07:33 -0300

    configura AgrixAplication

--- src/main/java/com/betrybe/agrix/AgrixApplication.java
+++ src/main/java/com/betrybe/agrix/AgrixApplication.java
@@ -2,11 +2,17 @@ package com.betrybe.agrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
 
 /**
  * Application main class.
  */
 @SpringBootApplication
+@EntityScan("com.betrybe.agrix.models.entities")
+@EnableJpaRepositories("com.betrybe.agrix.models.repositories")
+@Component("com.betrybe.agrix")
 public class AgrixApplication {
 
   public static void main(String[] args) {
